ee7dd63b6674bde5253797a7dfac47ecaaaec13a
Jeevan-12 <jeevangowdabjeeva@gmail.com>
2023-07-17 17:10:04 +0530

    new cors

--- index.js
+++ index.js
@@ -11,12 +11,21 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 // app.use(cors());
-app.use(
-  cors({
-    origin:
-      'https://64b51f4c985a640db2c80593--comforting-cobbler-51cac9.netlify.app/',
-  })
-);
+// app.use(
+//   cors({
+//     origin:
+//       'https://64b51f4c985a640db2c80593--comforting-cobbler-51cac9.netlify.app/',
+//   })
+// );
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('This server is reserved for prettycash application');
